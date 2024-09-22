2c5c7e1ca57cf276047a4e8f6d3a2e2703693636
miamanning <mmanning@terpmail.umd.edu>
2024-09-22 16:18:02 -0400

    External .js file added and completed JS functions

--- lab4.js
+++ lab4.js
@@ -1,4 +1,29 @@
 function showAlert() {
-    // Not returning a value since function doesn't require var
+    // Not returning a value since required name field on form ensures true or default
     alert('Hi '+ document.nameFun.user.value + '!');
+}
+
+function validateText() {
+    const validateRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    if (validateRegex.test(document.textFun.textTest.value)) {
+        // Will only enter this if bracket if condition is true
+        alert('How about...nah (Psst- your text contains a special character!)');
+        return false; 
+    } else {
+        // Return out that text is valid and contains no special chars
+        return true;
+    }
+}
+
+function addText() {
+    var titleText = document.getElementById('pageTitle');
+    titleText.innerHTML = 'INST377 - Lab 4 - Add Text'; 
+}
+
+function changeColor() {
+    if (document.body.style.backgroundColor == 'lightblue') {
+        const colorState = document.body.style.backgroundColor = 'green';
+    } else {
+        const colorState = document.body.style.backgroundColor = 'lightblue';
+    }
 }
